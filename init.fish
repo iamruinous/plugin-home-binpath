616624824dd221016cce954a2fee4bf3a68a18f3
Jade Meskill <jade.meskill@gmail.com>
2017-11-02 20:01:33 -0700

    remove init event

--- init.fish
+++ init.fish
@@ -1,5 +1,4 @@
-function init -a path --on-event init_home-binpath
-  set -l home_bin_path "$HOME/.bin"
-  contains -- $home_bin_path $PATH
-    or set -gx PATH $home_bin_path $PATH
-end
+set -l home_bin_path "$HOME/.bin"
+
+contains -- $home_bin_path $PATH
+  or set -gx PATH $home_bin_path $PATH
